e5e087d9ca4be63e75b2cb23c0b189d1927cab26
lena <lenarichards2234@gmail.com>
2024-11-14 01:36:47 +0000

    sending css and javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    const text = document.getElementById("text");
+    text.style.color = "red";
+});
--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,40 @@ fn main() {
         
         let mut tokens = line.split_whitespace();
         if tokens.next().unwrap() == "GET" {
-            let confirmation = "HTTP/1.1 200 OK";
-            let content = fs::read_to_string("index.html").expect("failed to read file");
-            let length = content.len();
+            let file = tokens.next().unwrap();
+            println!("{}", file);
+            match file{
+                "/" => {
+                    let confirmation = "HTTP/1.1 200 OK";
+                    let content = fs::read_to_string("index.html").expect("failed to read file");
+                    let length = content.len();
 
-            let response = format!("{confirmation}\r\nContent-Length: {length}\r\n\r\n{content}");
-            let _ = stream.write(&response.into_bytes());
+                    let response = format!("{confirmation}\r\nContent-Type: text/html\r\nContent-Length: {length}\r\n\r\n{content}");
+                    let _ = stream.write(&response.into_bytes());
+                }
+                "/style.css" => {
+                    let confirmation = "HTTP/1.1 200 OK";
+                    let content = fs::read_to_string("style.css").expect("failed to read file");
+                    let length = content.len();
+
+                    let response = format!("{confirmation}\r\nContent-Type: text/css\r\nContent-Length: {length}\r\n\r\n{content}");
+                    let _ = stream.write(&response.into_bytes());
+                }
+                "/script.js" => {
+                    let confirmation = "HTTP/1.1 200 OK";
+                    let content = fs::read_to_string("script.js").expect("failed to read file");
+                    let length = content.len();
+
+                    let response = format!("{confirmation}\r\nContent-Type: text/js\r\nContent-Length: {length}\r\n\r\n{content}");
+                    let _ = stream.write(&response.into_bytes());
+                }
+                _ => {
+                    let error = "HTTP/1.1 404 NOT FOUND";
+                    let content = fs::read_to_string("404.html").expect("failed to read file");
+                    let response = format!("{error}\r\nContent-Type: text/html\r\nContent-Length: {0}\r\n\r\n{content}", content.len());
+                    let _ = stream.write(&response.into_bytes());
+                }
+            }
         }
     }
 }
